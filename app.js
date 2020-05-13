a60c08d17313eaf8a92a5257bcd12db61e348938
Christian Matos <chris@Christians-MacBook.local>
2020-05-13 09:29:14 -0400

    midway

--- app.js
+++ app.js
@@ -1,100 +1,120 @@
-let index = 0 
+let index = 0
 let trivia;
 async function fetchData() {
-  const url = "https://opentdb.com/api.php?amount=10&category=15"
+  const url = "https://opentdb.com/api.php?amount=10&category=15&type=multiple"
 
   try {
     const response = await axios.get(url)
- trivia = response.data.results
+    trivia = response.data.results
     game()
-   } catch (error) {
+  } catch (error) {
     console.log(`${error}`)
   }
 }
 
 let start = document.querySelector('.start')
 let questionDiv = document.querySelector('.Question')
+let answerDiv = document.querySelector('.allbuttons')
 start.addEventListener('click', fetchData)
 
 function game() {
   questionDiv.innerHTML = ``;
+  answerDiv.innerHTML = ``;
   console.log(trivia)
-  function displayAnswers(question) {
-    //store the correct and incorrect answers at put into an array for shuffling
-    let correctAnswer = question[0].correct_answer
-    const incorrectAnswer1 = question[0].incorrect_answers[1]
-    const incorrectAnswer2 = question[0].incorrect_answers[2]
-    const incorrectAnswer3 = question[0].incorrect_answers[3]
-    const arrayAnswers = [correctAnswer, incorrectAnswer1, incorrectAnswer2, incorrectAnswer3]
-    
-    //for multiple choice quesitons, the answers always need to presented in a random order
-    if (question[0].type === 'multiple') {
-      
-      // funciton that shuffles they array of answer options created above
-      function shuffle(array) {
-        let currentIndex = array.length, temporaryValue, randomIndex;
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
-          // Pick a remaining element...
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex -= 1;
-          // And swap it with the current element.
-          temporaryValue = array[currentIndex];
-          array[currentIndex] = array[randomIndex];
-          array[randomIndex] = temporaryValue;
-        }
-        return array;
-      }
+  // function displayAnswers(question) {
+  //store the correct and incorrect answers at put into an array for shuffling
+  let correctAnswer = trivia[index].correct_answer
+  let incorrectAnswer1 = trivia[index].incorrect_answers[0]
+  let incorrectAnswer2 = trivia[index].incorrect_answers[1]
+  let incorrectAnswer3 = trivia[index].incorrect_answers[2]
+  let arrayAnswers = [correctAnswer, incorrectAnswer1, incorrectAnswer2, incorrectAnswer3]
 
-    //call the shuffle function and store results in an array
-    const shuffledArray = shuffle(arrayAnswers)
+  //for multiple choice quesitons, the answers always need to presented in a random order
+  // if (question[0].type === 'multiple') {
 
-     //udpate the HTML using the shuffled answer options.
-     $('#answers').html(
-      //display answer options in radio buttons
-
-
-    // Create array of questions
-
-
-    // Shuffle array
-
-
-    // Include shuffled array into HTML somehow (below)
- `
-  <div class="card"">
-    <h3>${trivia[0].question}</h3>
-
-    <input type="one" id="choice-1" class="answer" value='${shuffledArray[0]}'>
-    <label for="1" id="1">${shuffledArray[0]}</label><br><br>
-    <input type="two" id="choice-2" class="answer" value='${shuffledArray[1]}'>
-    <label for="2" id="2">${shuffledArray[1]}</label><br><br>
-    <input type="three" id="choice-3" class="answer" value='${shuffledArray[2]}'>
-    <label for="3" id="3">${shuffledArray[2]}</label><br><br>
-    <input type="four" id="choice-4" class="answer" value='${shuffledArray[3]}'>
-    <label for="4" id="4">${shuffledArray[3]}</label>
-    <br><br>
-    <button id= "allbuttons">Submit</button>
-    <div id = "next"></div>
-  `
-)
-} else {
-//for true/false questions, no shuffling or randomization is needed. these options will always
-//be presented as True and False in that order
-$('#answers').html(
-`
-    <input type="one" id="choice-1" class="answer" value="True">
-    <label for="1" id="1">True</label><br><br>
-    <input type="two" id="choice-2" class="answer" value="False">
-    <label for="2" id="2">False</label><br><br>
-    <br><br>
-    <button id= "submit-answer">Submit</button>
-    <div id = "next"></div>
-  `
- 
-)
+  // funciton that shuffles they array of answer options created above
+  function shuffle(array) {
+    let currentIndex = array.length-1
+    let temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
   }
-  `;
+
+  //call the shuffle function and store results in an array
+  let shuffledArray = shuffle(arrayAnswers)
+
+  //udpate the HTML using the shuffled answer options.
+
+  //display answer options in radio buttons
+
+
+  // Create array of questions
+
+
+  // Shuffle array
+
+
+  // Include shuffled array into HTML somehow (below)
+
+
+  questionDiv.innerHTML = `<h3>${trivia[index].question}</h3>`
+
+  for (let i = 0; i < shuffledArray.length; i++) {
+    let button = document.createElement('button')
+    button.classList.add("answerButton")
+    button.innerText = shuffledArray[i]
+    answerDiv.append(button)
+    button.addEventListener('click', () => {
+      game()
+    })
+  
   }
-  </div >
+
+index += 1
 }
+function showScores() {
+  let gameOverHTML = "<h1>Result</h1>";
+  gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+  let element = document.getElementById("Question");
+  element.innerHTML = gameOverHTML;
+}
+
+
+
+let fullReset = document.getElementById('fullReset');
+
+    fullReset.addEventListener('click', function(e) {
+      location.reload();
+    }, false);
+
+
+
+
+
+  // function playGame() {
+  //   let deck = shuffle(buildDeck())
+  //   let playerName = greet()
+  //   let score = 0 
+  //   let currentCard = deck.pop()
+
+  //   while (score > 5) {
+  //     let nextCard = deck.pop()
+  //     if (guess('h', 'l')) {
+  //       console.log(`${score += 1} Congrulations you've scored 1 point higher`)
+  //     } else {
+  //     console.log(`sorry you were wrong. You recive zero points!`)
+  //     }
+  //     currentCard = nextCard
+  //   }
+  //   (deck.length === 0) ? console.log("you have lost!") : console.log("you have won")
+
+  // }
